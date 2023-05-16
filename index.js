fb90d449b9626f27c733a203d950f4d20fd63eef
LucasRomanato0 <20.00313-7@maua.br>
2023-05-16 20:24:10 -0300

    api de cadastro e login feita

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function checkLoginAndPassword(req, res, next) {
+  if (!req.body.login || !req.body.password) {
+    return res.status(400).json({ error: "campo obrigatorio faltando." });
+  }
+
+  return next();
+}
+
+app.post("/cadastro", checkLoginAndPassword, (req, res) => {
+  const { login, email, phone, password } = req.body;
+  const newUser = {
+    login: login,
+    email: email,
+    phone: phone,
+    password: password,
+  };
+  const found = users.find((user) => user.login == login);
+
+  if (!found) {
+    users.push(newUser);
+    console.log("user criado");
+    return res.json({ message: "usuario criado" });
+  } else {
+    return res.json({
+      message: `usuário com login '${login}' já existente`,
+    });
+  }
+});
+
+app.post("/login", checkLoginAndPassword, (req, res) => {
+  const { login, password } = req.body;
+  const found = users.find(
+    (user) => user.login == login && user.password == password
+  );
+
+  if (!found) {
+    return res.json({ message: "usuario nao encontrado" });
+  } else {
+    return res.json({ message: "login realizado com sucesso" });
+  }
+});
+
+app.get("/get-user/:index", (req, res) => {
+  const { index } = req.params;
+
+  console.log("index: " + index);
+
+  return res.json(users[index]);
+});
+
+app.get("/get-user", (req, res) => {
+  return res.json(users);
+});
+
+app.put("/update-user/:index", (req, res) => {
+  const { index } = req.params;
+  const { login, email, password } = req.body;
+
+  if (login !== undefined) {
+    users[index].login = login;
+    console.log("login atualizado");
+  }
+  if (email !== undefined) {
+    users[index].email = email;
+    console.log("email atualizado");
+  }
+  if (password !== undefined) {
+    users[index].password = password;
+    console.log("senha atualizada");
+  }
+
+  return res.json({ message: "usuario atualizado" });
+});
+
+app.delete("/delete-user/:index", (req, res) => {
+  const { index } = req.params;
+
+  console.log("User deletado: " + users[index].login);
+
+  users.splice(index, 1);
+  return res.json({
+    message: `User deletado com sucesso!`,
+  });
+});
+
+app.listen(3000);
