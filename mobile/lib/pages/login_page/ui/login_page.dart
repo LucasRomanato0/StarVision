9f21c079200c129487c31472deae9d0b6320cc1a
LucasRomanato0 <lucasromanato34@gmail.com>
2023-10-04 11:08:17 -0300

    finalizando tela de login

--- mobile/lib/pages/login_page/ui/login_page.dart
+++ mobile/lib/pages/login_page/ui/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/shared/app_colors.dart';
+import 'package:mobile/widgets/botao_amarelo.dart';
 import 'package:mobile/widgets/container_principal.dart';
 import 'package:mobile/widgets/text_field_widget.dart';
 
@@ -14,11 +15,11 @@ class LoginPage extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
+            children: [
               ContainerPrincipal(
                 loginOrRegister: 1,
                 children: [
-                  Padding(
+                  const Padding(
                     padding: EdgeInsets.only(top: 25, bottom: 10),
                     child: TextFieldWidget(
                       hintText: 'email@teste.com',
@@ -29,7 +30,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
-                  TextFieldWidget(
+                  const TextFieldWidget(
                     hintText: '*******',
                     labelText: 'Senha',
                     obscureText: true,
@@ -38,6 +39,32 @@ class LoginPage extends StatelessWidget {
                       color: Colors.white,
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60),
+                    child: BotaoAmarelo(
+                      onPressed: () {},
+                      text: 'Login',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Não possui conta?',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Cadastre-se',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ],
