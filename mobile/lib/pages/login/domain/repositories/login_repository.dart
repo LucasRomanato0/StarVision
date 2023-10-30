21dc985700fe86a6c21c7dbd24261c2a91daf27e
LucasRomanato0 <lucasromanato34@gmail.com>
2023-10-30 09:59:28 -0300

    criando datasource do login

--- mobile/lib/pages/login/domain/repositories/login_repository.dart
+++ mobile/lib/pages/login/domain/repositories/login_repository.dart
@@ -2,5 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:mobile/pages/login/domain/entities/user_entity.dart';
 
 abstract class LoginRepository {
-  Future<Either<Exception, UserEntity>> login({String login, String senha});
+  Future<Either<Exception, UserEntity>> login({
+    required String login,
+    required String senha,
+  });
 }
